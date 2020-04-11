0a41aacfe42a687f6c06e47aeb70f1a33bab1c26
Samuel <samuel.santos134@etec.sp.gov.br>
2020-04-11 17:16:38 -0300

    Adicionando funcionalidade listar repositorios, criar post e deletar post

--- src/App.js
+++ src/App.js
@@ -1,29 +1,112 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api.js'
 
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setrepositories] = useState([]);
+
+  const [tecnologias, setTecnologias] = useState([]);
+
+  const [tecnologia, setTecnologia] = useState("");
+
+  const [titulo, setTitulo] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    api.get("/repositories").then(response => {
+      setrepositories(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("/repositories", {
+      title : titulo,
+      url : url,
+      techs : tecnologias
+    });
+
+    setrepositories([response.data, ...repositories])
+
+    setTitulo("");
+    setUrl("");
+    setTecnologias([]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`)
+
+    const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+    var repos = [...repositories];
+
+    repos.splice(repoIndex, 1);
+
+    setrepositories(repos)
+
+  }
+
+  function handleAddTecnologia() {
+    setTecnologias([...tecnologias, tecnologia]);
+    setTecnologia("");
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+<div>
+        <input type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} placeholder="Título..."></input>
+      </div>
+      <div>
+        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Url..."></input>
+      </div>
+      <div>
+        <input type="text" value={tecnologia} onChange={(e) => setTecnologia(e.target.value)} placeholder="Tecnologia..."></input>
+
+        <button onClick={handleAddTecnologia}>+</button>
+      </div>
+      <h3>
+        Tecnologias:
+      </h3>
+      <ul>
+        {
+          tecnologias.map((tec, index) => <li key={index}>{index}- {tec}</li>)
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
+
+
+      <ul data-testid="repository-list">
+        {
+          repositories.map(repositorio => {
+            return (
+              <li key={repositorio.id}>
+                <div>
+                {repositorio.title}
+                <div>
+                {repositorio.techs.length > 0 ? <h3>Tecnologias: </h3> : null}
+                </div>
+                <div>
+                <ul>
+                {
+                  repositorio.techs.map((tecn, index) => (<li key={index}>{index}- {tecn}</li>))
+                }
+                </ul>
+                </div>
+                <button onClick={() => handleRemoveRepository(repositorio.id)}>
+                  Remover
+                </button>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
+
+      
     </div>
   );
 }
